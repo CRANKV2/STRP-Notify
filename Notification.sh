510cc2cf936dee62bbbb476891c934f7002b40a3
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2023-12-25 23:44:37 +0100

    Aktualisieren von Notification.sh

--- Notification.sh
+++ Notification.sh
@@ -1,8 +1,11 @@
 #!/system/bin/sh
 
 su -lp 2000 -c "cmd notification post -S bigtext -t ' 𝙈𝘼𝘿𝙀 𝘽𝙔 @STRPxDEVS ' tag '━━━━━━━━━━━━
-Just testing new Real-Time
+Welcome to our new Real-Time
 Notifications!
+We will keep You Updated!
+Regards,
+@CRANKV2 | STRPxDEVS
 ━━━━━━━━━━━━
 𝙅𝙊𝙄𝙉 𝙎𝙏𝙍𝙋𝙭𝙐𝙉𝙄𝙑𝙀𝙍𝙎𝙀 𝙊𝙣 𝙏𝙚𝙡𝙚𝙜𝙧𝙖𝙢
 𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔 𝙎𝙏𝙍𝘼𝙏𝙊𝙎𝙋𝙃𝙀𝙍𝙀'" >/dev/null 2>&1;
